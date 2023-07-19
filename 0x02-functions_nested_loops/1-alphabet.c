486d7dde91e0b8d9b058dee5fe121e93783b9872
shaleentn <shaleentn@gmail.com>
2023-07-19 04:39:09 -0700

    Letter output

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+  *print_alphabet - output alphabet in lowercase
+  *
+  *Return: void
+  */
+void print_alphabet(void)
+{
+	char s;
+
+	for (s = 'a'; s <= 'z'; s++)
+		_putchar(s);
+	_putchar('\n');
+}
+

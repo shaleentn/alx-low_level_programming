cf7313695bc52b3d35b6657cf2b224def09545ae
shaleentn <shaleentn@gmail.com>
2023-07-21 12:26:10 -0700

    again!

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -10,37 +10,20 @@
 
 int largest_number(int a, int b, int c)
 {
-	int largest;
+    int largest;
 
-	if (a > b && b > c)
-	{
-		largest = a;
-	}
-	else if (c > b && b > a)
-	{
-		largest = c;
-	}
-	else if (b > c && c > a)
-	{
-		largest = b;
-	}
-	else if (a > c && c > b)
-	{
-		largest = a;
-	}
-	else if (b > a && a > c)
-	{
-		largest = b;
-	}
-	else if (c > a && a > b)
-	{
-		largest = c;
-	}
-	else
-	{
-		largest = c;
-	}
+    if (a >= b && a >= c)
+    {
+        largest = a;
+    }
+    else if (b >= a && b >= c)
+    {
+        largest = b;
+    }
+    else
+    {
+        largest = c;
+    }
 
-	return (largest);
+    return (largest);
 }
-

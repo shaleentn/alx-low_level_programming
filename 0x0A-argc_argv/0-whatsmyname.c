441d5760cfefb4a197f6db0296baea3eeccd23a5
shaleentn <shaleentn@gmail.com>
2023-08-04 04:50:59 -0700

    Writing the first argument

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+  *main - checks the main code
+  *@argc:the number of arguments that are going to be passed
+  *@argv: pointer to the array of strings
+  *Return: Always 0
+  */
+
+int main(__attribute__((unused))int argc, char *argv[])
+{
+	int i;
+
+	i = 0;
+	printf("%s\n", argv[i]);
+	return (0);
+}

0b9ac81466800e43279a29c477ffca1ea722c282
shaleentn <shaleentn@gmail.com>
2023-08-08 01:15:25 -0700

    concatenating strings

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -22,6 +22,8 @@ char *str_concat(char *s1, char *s2)
 	while (s2[j] != '\0')
 		j++;
 	s = malloc(sizeof(char) * (i + j + 1));
+	if (s == NULL)
+		return (NULL);
 
 	for (i = 0; s1[i] != '\0'; i++)
 		s[i] = s1[i];

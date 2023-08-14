1806db95dc28a3fc7443db7d5a8e4cbb6d4975f1
shaleentn <shaleentn@gmail.com>
2023-08-14 12:38:39 -0700

    Structures,typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,14 @@
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+/**
+  *struct dog - type describing dog
+  *@name: pointer to the name to initialize
+  *@age: age of the dog
+  *@owner: pointer to owner to initilize
+  */
+struct dog{
 	char *name;
 	char *owner;
 	float age;
 };
+#endif

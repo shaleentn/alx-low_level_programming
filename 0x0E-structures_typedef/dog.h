371fa5a3a8cd16fa7fe01af601924c043ab4b972
shaleentn <shaleentn@gmail.com>
2023-08-14 14:09:41 -0700

    Printing struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -11,13 +11,15 @@ struct dog
 	char *name;
 	float age;
 	char *owner;
-}
+};
 /**
   *dog_t - typedef of the struct dog
   */
 typedef struct dog dog_t;
+
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+
 #endif

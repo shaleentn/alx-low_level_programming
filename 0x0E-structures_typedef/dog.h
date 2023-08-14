e7e139d7d4528f10061d58b11ee540d5a10c56b1
shaleentn <shaleentn@gmail.com>
2023-08-14 13:59:03 -0700

    Defining odf a astructure

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,25 +1,23 @@
 #ifndef DOG_H
 #define DOG_H
 /**
-  *struct dog - type describing dog
-  *@name: pointer to the name to initialize
+  *struct dog - defines struct dog
+  *@name: name of the struct
   *@age: age of the dog
-  *@owner: pointer to owner to initilize
+  *@owner: owner of the dog
   */
 struct dog
 {
 	char *name;
-	char *owner;
 	float age;
-};
+	char *owner;
+}
 /**
-  *dog_t - typedef for struct dog
+  *dog_t - typedef of the struct dog
   */
 typedef struct dog dog_t;
-
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
-
 #endif

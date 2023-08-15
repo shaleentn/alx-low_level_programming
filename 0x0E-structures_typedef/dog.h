3dc7d74c406bdedebf33d2f79035c2b069da734f
shaleentn <shaleentn@gmail.com>
2023-08-15 13:45:55 -0700

    Doggy

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -14,4 +14,13 @@ struct dog
 	char *owner;
 };
 
+/**
+  *dog_t - typedef of the struct dog
+  */
+
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif /*DOG_H*/
